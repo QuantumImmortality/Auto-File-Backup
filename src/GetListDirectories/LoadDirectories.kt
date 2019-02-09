9ee80bdbddee218a1bfd29a46a06bcd4a99a78c4
QuantumImmortality <lorenzo.monsif1@gmail.com>
2019-02-09 20:04:25 +1100

    Loads the file of directories
    Loads the file containing the list of directories that are separated by new lines

--- src/GetListDirectories/LoadDirectories.kt
+++ src/GetListDirectories/LoadDirectories.kt
@@ -1,5 +1,7 @@
 package GetListDirectories
 
+import Logging.LogLevel
+import Logging.Logger.Companion.writeLogMessage
 import java.io.File
 
 internal class LoadDirectories {
@@ -13,11 +15,22 @@ internal class LoadDirectories {
         file.createNewFile()
     }
 
+    /**
+     * Load the file containing the list of directories to backup
+     * separated by new lines
+     * @return List of the directories
+     */
     fun loadFile(): List<String> {
 
         val dirs: MutableList<String> = mutableListOf()
 
         val file = File(fileName)
+
+        if(!file.exists())
+            writeLogMessage("Directories file doesn't exist", LogLevel.ERROR)
+        if(file.length() == 0L)
+            writeLogMessage("Directories file doesn't contain any directories to backup", LogLevel.WARN)
+
         file.forEachLine { dirs.add(it) }
 
         return dirs
