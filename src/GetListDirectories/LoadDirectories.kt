d4da97be2a427bcb9841867ac17adb25b87c1c9d
QuantumImmortality <lorenzo.monsif1@gmail.com>
2019-02-09 19:41:25 +1100

    Added Read File Capacity
    A file can now be read line by line with explicit directories on each line

--- src/GetListDirectories/LoadDirectories.kt
+++ src/GetListDirectories/LoadDirectories.kt
@@ -4,11 +4,22 @@ import java.io.File
 
 internal class LoadDirectories {
 
+    private var fileName = "Directories.txt"
+    /**
+     * If file doesn't exist, create it
+     */
     fun createFile(){
-
-        val fileName = "Directories.txt"
         var file = File(fileName)
         file.createNewFile()
+    }
 
+    fun loadFile(): List<String> {
+
+        val dirs: MutableList<String> = mutableListOf()
+
+        val file = File(fileName)
+        file.forEachLine { dirs.add(it) }
+
+        return dirs
     }
 }
--- src/Manager.kt
+++ src/Manager.kt
@@ -6,6 +6,14 @@ fun main(args : Array<String>){
     var dir = LoadDirectories()
     dir.createFile()
 
-    Logger.writeLogMessage("logAStatment", Logging.LogLevel.DEBUG)
+    //Logger.writeLogMessage("logAStatment", Logging.LogLevel.DEBUG)
+    var dirs = dir.loadFile()
+
+    dirs.forEach{println(it)}
 
 }
+/*
+TODO Read txt file with each line have an explicit dir of file to copy
+TODO Target dir to put the copy, initially put to same, then separate folders
+
+ */
